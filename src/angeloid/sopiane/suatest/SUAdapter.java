10b3bc4214421a5f945b92d140f78eddabf6abda
Dong Gil, Seo <sirospace@sirospace.com>
2013-03-12 01:11:38 +0900

    2013.03.11 Reseach : SUAdpater
    Ghana, Sopiane.
    Make a New Source about SUAdapter!

--- src/angeloid/sopiane/suatest/SUAdapter.java
+++ src/angeloid/sopiane/suatest/SUAdapter.java
@@ -1,24 +1,61 @@
 package angeloid.sopiane.suatest;
 
-import java.io.IOException;
+// 버퍼(임시 메모리)를 쓰는 용도
+// Buffer (temporary memory) writing purposes
+import java.io.BufferedReader;
+
+// 버퍼를 읽는 용도
+// Buffer reading purposes
+import java.io.InputStreamReader;
 
 import android.app.Activity;
-import android.content.Intent;
+
+// 로그캣을 쓰는 용도
+// Logcat writing purposes
+import android.util.Log;
 
 public class SUAdapter extends Activity {
 
-	void SUAdapterMain() {
-	final int SUPERUSER_REQUEST = 2323;
-	Intent intent = new Intent("android.intent.action.superuser");
-	intent.putExtra("name", "SUAdpaterTest");
-	intent.putExtra("packagename", "angeloid.sopiane.suatest");
-	startActivityForResult(intent, SUPERUSER_REQUEST); 
-	}
-	
-	void SUCommandReceiver(String sucommand) throws IOException, InterruptedException {
+	void SUCommandReceiver(){
+		// MainActivity.java에서 받은 데이터를 불러온다
+		// To bring up the data received from the MainActivity.java
 		String sucommand_receiver = getIntent().getStringExtra("sucommand");
-		Runtime.getRuntime().exec("su -c" + sucommand_receiver);
-		Thread.sleep(4000);
-		sucommand_receiver = "";
+		// Su권한을 사용하기 위해 선언한다
+		// Su permission to use the declaration
+		 Runtime sucommand_runtime = Runtime.getRuntime(); 
+		// 프로세스를 선언한다
+		// Process to use the declaration
+	     Process processcommand; 
+	     try {
+	    	 // SU권한을 얻기 위해 받은 데이터를 수정한다
+	    	 // SU permission to obtain the received data is modified
+	 		String[] sucommand = new String[] {
+					"su",
+					"-c" + sucommand_receiver };
+	 		// 수정한 데이터를 실행한다
+	 		// Modify data, run
+	    	 processcommand = sucommand_runtime.exec(sucommand); 
+	    	// 버퍼를 사용한다
+	    	// Use Buffer
+	    	 BufferedReader br = new BufferedReader(new InputStreamReader(processcommand.getInputStream()));
+	    	 // 문구가 오류를 일으키거나 버퍼가 문제 있을 때 디버깅으로 알려준다.
+	    	 // Tells the error terms can cause debugging problems when the buffer is.
+             String line ; 
+             while ((line = br.readLine()) != null) { 
+             	Log.i("SUAdapter",line);
+             } 
+             // 받은 데이터를 나중에 다시 사용하기 위해 지운다
+             // Received data in order to use it again in the future, erase
+             sucommand_receiver = "";
+             // 오류가 일어났을 때 처리한다.
+             // Processed when the error occurred.
+	     } catch (Exception e) {
+	    	 // 강종 메세지를 띄운다
+	    	 // Bring up the steel grade message
+	    	 e.fillInStackTrace();
+	    	 // 로그캣에 오류 메세지를 보여준다
+	    	 // Show an error message in the log Cat
+	    	 Log.e("SUAdapter", "SOME ERROR(S) Detected in SUAdapter in SUAdapter.java!");
+	     }
 	}
 }
