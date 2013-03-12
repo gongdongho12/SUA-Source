03c98925898ea5feeea0a4b0aea22a0c8b097305
Dong Gil, Seo <sirospace@sirospace.com>
2013-03-12 23:40:23 +0900

    Now, Available Fragment! (or v4.fragment)

--- src/angeloid/sopiane/suatest/MainActivity.java
+++ src/angeloid/sopiane/suatest/MainActivity.java
@@ -16,17 +16,23 @@ public class MainActivity extends Activity {
 	// StartAdapter OpenSource
 	// For SUPERAdapter , UnZipThread
 	void StartAdapter (Context con, String command_adapter) throws InterruptedException{
+		// 2013.03.12 연구주제 >>> 
 		UnZipThread.StartThread(MainActivity.this);
 		Thread.sleep(3000);
 		StringBuilder SA = new StringBuilder();
 		SA.append("mount -o rw,remount /system;");
 		SA.append("busybox touch /system/SuperAdapterTest;");
-		SA.append("busybxo chmod 777 /system/SuperAdapterTest;");
+		SA.append("busybox chmod 777 /system/SuperAdapterTest;");
 		// Thread.sleep(Wait Device)
 		Thread.sleep(4000);
+		
+		// if use Activity or etc, (Not a Fragment), use this String.
 		String Command = SA.toString();
 		Intent intent = new Intent(MainActivity.this, SUAdapter.class); 
 		intent.putExtra("sucommand", Command); 
 		startActivity(intent);
+		
+		// if use Fragment or v4.Fragment, Use this String.
+		SUAdapter.sucommand_receiver_fragment = SA.toString();
 	}
 }
