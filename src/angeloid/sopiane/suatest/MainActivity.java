d689bb225cb981b34c27a2a5c6e82b7bc851574c
Dong Gil, Seo <sirospace@sirospace.com>
2013-03-13 00:43:14 +0900

    2013.03.13 Rearch : MainActivity

--- src/angeloid/sopiane/suatest/MainActivity.java
+++ src/angeloid/sopiane/suatest/MainActivity.java
@@ -1,11 +1,17 @@
 package angeloid.sopiane.suatest;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import android.util.Log;
+
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+import android.app.ProgressDialog;
 
 public class MainActivity extends Activity {
+
+	Handler mHandler;
+	ProgressDialog dialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,26 +19,35 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	// StartAdapter OpenSource
-	// For SUPERAdapter , UnZipThread
-	void StartAdapter (Context con, String command_adapter) throws InterruptedException{
-		// 2013.03.12 연구주제 >>> 
-		UnZipThread.StartThread(MainActivity.this);
-		Thread.sleep(3000);
-		StringBuilder SA = new StringBuilder();
-		SA.append("mount -o rw,remount /system;");
-		SA.append("busybox touch /system/SuperAdapterTest;");
-		SA.append("busybox chmod 777 /system/SuperAdapterTest;");
-		// Thread.sleep(Wait Device)
-		Thread.sleep(4000);
-		
-		// if use Activity or etc, (Not a Fragment), use this String.
-		String Command = SA.toString();
-		Intent intent = new Intent(MainActivity.this, SUAdapter.class); 
-		intent.putExtra("sucommand", Command); 
-		startActivity(intent);
-		
-		// if use Fragment or v4.Fragment, Use this String.
-		SUAdapter.sucommand_receiver_fragment = SA.toString();
+	private void DialogProgress(boolean close) {
+		if (!close) {
+			dialog = ProgressDialog.show(MainActivity.this, "", "Loading..",
+					true);
+			Handler mHandler = new Handler();
+			mHandler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					DialogProgress(true);
+					StringBuilder SA = new StringBuilder();
+					SA.append("mount -o rw,remount /system;");
+					SA.append("busybox touch /system/SuperAdapterTest;");
+					SA.append("busybox chmod 777 /system/SuperAdapterTest;");
+					try {
+						SUAdapter.sucommand_receiver = SA.toString();
+						try {
+							SUAdapter.SUCommandReceiver();
+						} catch (Exception e) {
+							e.fillInStackTrace();
+							Log.e("MainActivity.this", "Some Error(s) detected in SUAdapter!!");
+						}
+					} catch (Exception e) {
+						e.fillInStackTrace();
+						Log.e("MainActivity.this", "Some Error(s) detected in StringBuilder!");
+					}
+				}
+			}, 20000);
+		} else {
+			dialog.dismiss();
+		}
 	}
 }
--- src/angeloid/sopiane/suatest/SUAdapter.java
+++ src/angeloid/sopiane/suatest/SUAdapter.java
@@ -8,23 +8,16 @@ import java.io.BufferedReader;
 // Buffer reading purposes
 import java.io.InputStreamReader;
 
-import android.app.Activity;
-
 // 로그캣을 쓰는 용도
 // Logcat writing purposes
 import android.util.Log;
 
-public class SUAdapter extends Activity {
-	
-	// SUAdapter Source(s)
+public class SUAdapter {
 
-	static String sucommand_receiver_fragment = "";
+	// SUAdapter Source(s)
 	static String sucommand_receiver = "";
 
-	void SUCommandReceiver() {
-		// MainActivity.java에서 받은 데이터를 불러온다
-		// To bring up the data received from the MainActivity.java
-		sucommand_receiver = getIntent().getStringExtra("sucommand");
+	public static void SUCommandReceiver() {
 		// Su권한을 사용하기 위해 선언한다
 		// Su permission to use the declaration
 		Runtime sucommand_runtime = Runtime.getRuntime();
@@ -64,50 +57,5 @@ public class SUAdapter extends Activity {
 					"SOME ERROR(S) Detected in SUAdapter in SUAdapter.java!");
 		}
 
-	}
-
-	void SUAdapter_Fragment() {
-		// MainActivity.java에서 받은 데이터를 불러온다
-		// To bring up the data received from the MainActivity.java
-		// Su권한을 사용하기 위해 선언한다
-		// Su permission to use the declaration
-		Runtime sucommand_runtime_fragment = Runtime.getRuntime();
-		// 프로세스를 선언한다
-		// Process to use the declaration
-		Process processcommand_fragment;
-		try {
-			// SU권한을 얻기 위해 받은 데이터를 수정한다
-			// SU permission to obtain the received data is modified
-			String[] sucommand = new String[] { "su",
-					"-c" + sucommand_receiver_fragment };
-			// 수정한 데이터를 실행한다
-			// Modify data, run
-			processcommand_fragment = sucommand_runtime_fragment
-					.exec(sucommand);
-			// 버퍼를 사용한다
-			// Use Buffer
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					processcommand_fragment.getInputStream()));
-			// 문구가 오류를 일으키거나 버퍼가 문제 있을 때 디버깅으로 알려준다.
-			// Tells the error terms can cause debugging problems when the
-			// buffer is.
-			String line;
-			while ((line = br.readLine()) != null) {
-				Log.i("SUAdapter_Fragment", line);
-			}
-			// 받은 데이터를 나중에 다시 사용하기 위해 지운다
-			// Received data in order to use it again in the future, erase
-			sucommand_receiver_fragment = "";
-			// 오류가 일어났을 때 처리한다.
-			// Processed when the error occurred.
-		} catch (Exception e) {
-			// 강종 메세지를 띄운다
-			// Bring up the steel grade message
-			e.fillInStackTrace();
-			// 로그캣에 오류 메세지를 보여준다
-			// Show an error message in the log Cat
-			Log.e("SUAdapter_Fragment",
-					"SOME ERROR(S) Detected in SUAdapter in SUAdapter.java!");
-		}
 	}
 }
